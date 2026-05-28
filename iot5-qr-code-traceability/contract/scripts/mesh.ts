b6cff6c17773eae5dcc17d4036de772e55a54724
NguIndependence <nguyenkhanh17112003@gmail.com>
2026-05-28 16:48:42 +0700

    feat: This class provides methods to manage a status token lifecycle on the Cardano blockchain using Mesh SDK and Plutus V3 scripts.

--- iot5-qr-code-traceability/contract/scripts/mesh.ts
+++ iot5-qr-code-traceability/contract/scripts/mesh.ts
@@ -11,6 +11,7 @@ import {
   Data,
   serializePlutusScript,
   type UTxO,
+  mConStr0,
 } from "@meshsdk/core";
 import blueprint from "../plutus.json";
 
@@ -219,24 +220,11 @@ export class MeshAdapter {
     return await this.fetcher.fetchAddressUTxOs(address, unit);
   };
 
-  protected metadataToCip68 = (metadata: any): Data => {
-    switch (typeof metadata) {
-      case "object":
-        if (metadata instanceof Array) {
-          return metadata.map((item) => this.metadataToCip68(item));
-        }
-        const metadataMap = new Map();
-        const keys = Object.keys(metadata);
-        keys.forEach((key) => {
-          metadataMap.set(key, this.metadataToCip68(metadata[key]));
-        });
-        return {
-          alternative: 0,
-          fields: [metadataMap, 1],
-        };
+  protected metadataToCip68 = (metadata: Record<string, string>): any => {
+    const results = Object.entries(metadata).map(([key, value]) =>
+      mConStr0([key, value]),
+    );
 
-      default:
-        return metadata;
-    }
+    return mConStr0([results, 1]);
   };
 }
